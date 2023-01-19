b4cc79aa4b432e9b13095ed107fb356f65d78e8e
Doug Carter <dougc85@gmail.com>
2023-01-19 15:58:51 -0800

    created initial user and post schemas

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  posts: [{
+    startingText: {
+      type: String,
+      required: true
+    },
+    numComments: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+    }
+  }],
+  pwResetToken: String,
+  pwResetTokenExp: Date,
+});
+
+module.exports = mongoose.model('User', userSchema);
