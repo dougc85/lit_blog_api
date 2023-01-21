cb3824b3ec542ce259bf22db27614c83b5e04160
Doug Carter <dougc85@gmail.com>
2023-01-20 17:42:34 -0800

    finished basic app.js setup

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const bcrypt = require('bcryptjs');
+
+const app = express();
+const mongoDB = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    let port = process.env.PORT || '3000';
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log('Cannot connect to DB');
+  })
